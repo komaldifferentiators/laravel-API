7d2dd186c728658b9e75f6c167b7194b92894c8d
komaldifferentiators <komal@differentiators.in>
2021-04-08 18:37:43 +0530

    Laravel API

---
+++ app/Http/Controllers/APIController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Registers;
+
+
+class APIController extends Controller
+{
+    public function getData(Request $request)
+    {
+    		return registers::all();
+    }
+
+    public function add(Request $request)
+    {
+    		$request->validate([
+                'f_name'        =>      'required',
+                'l_name'        =>      'required',
+                'email'         =>      'required|email|unique:registers',
+                'password'      =>      'required|min:6',
+                'phone_no'      =>      'required|max:10',
+                'subscription'  =>      'required',
+            ]);
+
+    		$registers = new registers;
+    		$registers->f_name = $request->f_name;
+    		$registers->l_name = $request->l_name;
+    		$registers->email = $request->email;
+    		$registers->password = md5($request->password);
+    		$registers->phone_no = $request->phone_no;
+    		$registers->subscription = $request->subscription;
+
+    		$result = $registers->save();
+    		if($result)
+    		{
+    			return ["result"=>"Data has been saved"];	
+    		}else
+    		{
+    			return ["result"=>"Add Operation failed!"];
+    		}
+
+    		
+    }
+
+
+    public function update(Request $request)
+    {
+    	$registers = Registers::find($request->id);
+    	$registers->f_name = $request->f_name;
+    		$registers->l_name = $request->l_name;
+    		$registers->email = $request->email;
+    		$registers->password = md5($request->password);
+    		$registers->phone_no = $request->phone_no;
+    		$registers->subscription = $request->subscription;
+
+    		$result = $registers->save();
+    		if($result)
+    		{
+    			return ["result"=>"Data has been updated"];	
+    		}else
+    		{
+    			return ["result"=>"Update Operation failed!"];
+    		}
+    }
+
+    public function delete(Request $request)
+    {
+    	$registers = Registers::find($request->id);
+    	$result = $registers->delete();
+    	if($result)
+    	{
+    		return ["result"=>"Data has been deleted"];
+    	}else
+    	{
+    		return ["result"=>"Delete operation failed!"];
+    	}
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\APIController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+Route::get("data",[APIController::class,'getData']);
+Route::post("add",[APIController::class,'add']);
+Route::put("update",[APIController::class,'update']);
+Route::delete("delete/{id}",[APIController::class,'delete']);
